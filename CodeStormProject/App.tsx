6b5807f822f5288e7ef75ff9351729b550357d0f
storyshiftcharadark <kauanbarcelos77@gmail.com>
2023-09-12 19:10:18 -0300

    commit do kalvan

--- CodeStormProject/App.tsx
+++ CodeStormProject/App.tsx
@@ -8,6 +8,8 @@ export default function App(){
   // Login ==  true(renderiza Home)
   let login = false
   if(!login) {
+
+    
     return(
       <SafeAreaView style={{flex: 1}}>
         <NavigationContainer>
