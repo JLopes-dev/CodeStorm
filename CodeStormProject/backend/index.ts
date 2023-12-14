1f7d719b33b9a07e470755b71e4400f835f3ef3e
JotaPe8 <joaopedrolopessardinha@gmail.com>
2023-12-14 12:29:19 -0300

    Update in package.json

--- CodeStormProject/backend/index.ts
+++ CodeStormProject/backend/index.ts
@@ -2,6 +2,7 @@ import { connect } from "mongoose";
 import DatabaseInput from "./core/database";
 import RouterInput from "./routes/router";
 import express from "express";
+import dotenv from 'dotenv';
 const runDatabase = new DatabaseInput(connect);
 
 
@@ -9,6 +10,9 @@ const app = express();
 const httpServer = new RouterInput(app);
 const port = parseInt(process.env.PORT!) || 3000;
 
+if(process.env.NODE_ENV === 'dev'){
+  dotenv.config({{path: './config/.env.dev'}})
+}
 runDatabase.runDB(process.env.DB_URI!).then(() => {
   httpServer.runServer(port);
 });
